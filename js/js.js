5710c599d1be5c2a7b4e9cc8ddc7fc6f1c33c96f
Kristine Dzumakajeva <kristinedzu@gmail.com>
2020-05-20 12:53:17 +0200

    about us page is finished

--- js/js.js
+++ js/js.js
@@ -1,10 +1,11 @@
+
+/* this is the photo gallery on about us page */
+
 $(document).ready(function () {
-
-
     $('.gallery').slick({
         dots: false,
-        prevArrow: true,
-        nextArrow: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
         autoplay: true,
     });
 });
